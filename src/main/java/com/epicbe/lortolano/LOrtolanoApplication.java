0530e7c2c32a1a91f46344f3a4a6a3da98816e80
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-05-12 10:56:36 +0200

    Initial commit

---
+++ src/main/java/com/epicbe/lortolano/LOrtolanoApplication.java
@@ -0,0 +1,13 @@
+package com.epicbe.lortolano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LOrtolanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LOrtolanoApplication.class, args);
+	}
+
+}
